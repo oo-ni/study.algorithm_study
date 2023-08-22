96379e4fa0fa6f10f3d3e6fe771c5a5b9fb1f30a
GeonWoo <69147082+Y-gw@users.noreply.github.com>
2023-08-22 17:45:01 +0900

    230822

--- basic100/6086.py
+++ basic100/6086.py
@@ -1,1 +1,11 @@
 # 6086 : [기초-종합] 거기까지! 이제 그만~
+n = int(input())
+temp = 0
+result = 0
+
+while True:
+    temp += 1
+    result += temp
+    if result >= n:
+        break
+print(result)
--- basic100/6087.py
+++ basic100/6087.py
@@ -1,1 +1,6 @@
 # 6087 : [기초-종합] 3의 배수는 통과
+n = int(input())
+
+for i in range(1, n+1):
+    if i%3 != 0:
+        print(i, end=' ')
