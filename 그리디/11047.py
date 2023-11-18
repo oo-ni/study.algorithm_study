bbe03f12e6c5971b45308af64234abad09089908
GeonWoo <gdbsrjsdn@gmail.com>
2023-11-18 22:59:19 +0900

    231118 11047 그리디

---
+++ 그리디/11047.py
@@ -0,0 +1,12 @@
+n, k = map(int, input().split())
+coin = [int(input()) for i in range(n)]
+result = 0
+
+for i in range(n-1, 0, -1):
+    if k < coin[i]:
+        continue
+    else:
+        result += (k//coin[i])
+        k = k%coin[i]
+
+print(result)
