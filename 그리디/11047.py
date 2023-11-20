e91100cb7efc9cbf290580b73aea78921020529a
GeonWoo <gdbsrjsdn@gmail.com>
2023-11-20 22:27:46 +0900

    231120 complete 11047

--- 그리디/11047.py
+++ 그리디/11047.py
@@ -2,7 +2,7 @@ n, k = map(int, input().split())
 coin = [int(input()) for i in range(n)]
 result = 0
 
-for i in range(n-1, 0, -1):
+for i in range(n-1, -1, -1):
     if k < coin[i]:
         continue
     else:
