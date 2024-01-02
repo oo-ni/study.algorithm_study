e594add4894b2d4acb6a54dfb93b966edae6d81d
GeonWoo <gdbsrjsdn@gmail.com>
2024-01-02 19:16:27 +0900

    240102 add 11650

---
+++ 정렬/11650.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+li = []
+for i in range(n):
+    [a, b] = map(int, input().split())
+    li.append([a, b])
+li.sort()
+for i in li:
+    print(i[0], i[1])
