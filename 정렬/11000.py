21cf8ad09398c61956fc34b38ba420e8a5c7005a
GeonWoo <gdbsrjsdn@gmail.com>
2024-01-08 21:20:05 +0900

    240108 add 1253 mod 11000

--- 정렬/11000.py
+++ 정렬/11000.py
@@ -21,26 +21,46 @@
 
 # print(len(room))
 
+# import heapq
+
+# n = int(input())
+
+# lecture_num = []
+# lecture_queue = []
+
+# for _ in range(n):
+#     start, end = map(int, input().split())
+#     lecture_num.append([start, end])
+
+# lecture_num.sort()
+
+# heapq.heappush(lecture_queue, lecture_num[0][1]) # 수업1의 끝나는 시간을 heap에 추가
+
+# for i in range(1, n): # 수업2부터 모든 강의에 대해 반복
+#     if lecture_num[i][0] < lecture_queue[0]:  # 현재 진행하는 강의의 시작시간이 우선순위 큐의 끝나는 시간 보다 작으면 현재 강의의 끝나는 시간을 우선순위 큐에 추가
+#         heapq.heappush(lecture_queue, lecture_num[i][1])
+#     else: # 겹치지 않는 경우
+#         heapq.heappop(lecture_queue)
+#         heapq.heappush(lecture_queue, lecture_num[i][1])
+
+# print(len(lecture_queue)) # 우선순위 큐에 들어있는 숫자의 개수
+
+import sys
 import heapq
+input = sys.stdin.readline
 
-n = int(input())
+if __name__ == "__main__":
+    n = int(input())
+    schedule_list = [list(map(int, input().split())) for _ in range(n)]
+    schedule_list.sort()
 
-lecture_num = []
-lecture_queue = []
+    pq = list()
+    # 종료시간 저장
+    heapq.heappush(pq, schedule_list[0][1])
 
-for _ in range(n):
-    start, end = map(int, input().split())
-    lecture_num.append([start, end])
+    for i in range(1, n):
+        if schedule_list[i][0] >= pq[0]:
+            heapq.heappop(pq)
+        heapq.heappush(pq, schedule_list[i][1])
 
-lecture_num.sort()
-
-heapq.heappush(lecture_queue, lecture_num[0][1]) # 수업1의 끝나는 시간을 heap에 추가
-
-for i in range(1, n): # 수업2부터 모든 강의에 대해 반복
-    if lecture_num[i][0] < lecture_queue[0]:  # 현재 진행하는 강의의 시작시간이 우선순위 큐의 끝나는 시간 보다 작으면 현재 강의의 끝나는 시간을 우선순위 큐에 추가
-        heapq.heappush(lecture_queue, lecture_num[i][1])
-    else: # 겹치지 않는 경우
-        heapq.heappop(lecture_queue)
-        heapq.heappush(lecture_queue, lecture_num[i][1])
-
-print(len(lecture_queue)) # 우선순위 큐에 들어있는 숫자의 개수
+    print(len(pq))
