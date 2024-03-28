0d71e9fb043b2c1df3a15ab3b430a129ec13edfd
GeonWoo <gdbsrjsdn@gmail.com>
2024-03-28 22:09:33 +0900

    240328 add 1966

--- programmers/43165.py
+++ programmers/43165.py
@@ -1,1 +1,17 @@
-# 타겟 넘버
+def solution(numbers, target):
+    n = len(numbers)
+    answer = 0
+
+    def dfs(idx, result):
+        if idx == n:
+            if result == target:
+                nonlocal answer
+                answer += 1
+            return
+        else:
+            dfs(idx + 1, result + numbers[idx])
+            dfs(idx + 1, result - numbers[idx])
+
+    dfs(0, 0)
+
+    return answer
