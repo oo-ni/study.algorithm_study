21946f307e0034027c03a53159bd60c8ef97ae44
ooni <gdbsrjsdn@gmail.com>
2024-06-25 01:10:34 +0900

    240624 test commit

---
+++ algorithm-java/src/math/bj10869.java
@@ -0,0 +1,18 @@
+package math;
+import java.util.Scanner;
+
+public class bj10869 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int A = in.nextInt();
+        int B = in.nextInt();
+
+        in.close();
+
+        System.out.println(A+B);
+        System.out.println(A-B);
+        System.out.println(A*B);
+        System.out.println(A/B);
+        System.out.println(A%B);
+    }
+}
