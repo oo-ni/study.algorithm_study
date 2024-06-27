03e418b0b6157d53ce9fbf04345c8b2cbe2afa11
ooni <gdbsrjsdn@gmail.com>
2024-06-28 01:43:02 +0900

    240628 add 5 49 819 937

--- algorithm-java/src/implementation/lt344.java
+++ algorithm-java/src/implementation/lt344.java
@@ -1,9 +1,12 @@
 package implementation;
 
-public class lt344 {
+class lt344 {
     public void reverseString(char[] s) {
+        // 인덱싱을 위한 변수
         int left = 0;
         int right = s.length -1;
+
+        // 스왑 로직
         while (left < right) {
             char temp = s[left];
             s[left] = s[right];
