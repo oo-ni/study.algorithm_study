d302f6a105e6d75defe4729eb15b390b398d5932
GeonWoo <gdbsrjsdn@gmail.com>
2024-07-11 22:55:41 +0900

    240711 add string

---
+++ algorithm-java/src/string/bj2743.java
@@ -0,0 +1,13 @@
+package string;
+
+import java.util.Scanner;
+
+public class bj2743 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String word = sc.next();
+
+        System.out.println(word.length());
+    }
+}
---
+++ algorithm-java/src/string/bj2744.java
@@ -0,0 +1,24 @@
+package string;
+
+import java.util.Scanner;
+
+public class bj2744 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String word = sc.next();
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (Character.isUpperCase(c)) {
+                sb.append(Character.toLowerCase(c));
+            } else {
+                sb.append(Character.toUpperCase(c));
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
