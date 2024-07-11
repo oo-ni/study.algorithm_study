49feee51763b47e528314150f1be66cbfb5b2cab
ooni <gdbsrjsdn@gmail.com>
2024-07-11 23:49:16 +0900

    240711

---
+++ algorithm-java/src/string/bj11654.java
@@ -0,0 +1,14 @@
+package string;
+
+import java.util.Scanner;
+
+public class bj11654 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int ch = sc.next().charAt(0);
+
+        System.out.println(ch);
+
+    }
+}
