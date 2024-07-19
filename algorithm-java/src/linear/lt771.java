45de39d1ef59d43a60917f867689f90dafb1b9b3
GeonWoo <gdbsrjsdn@gmail.com>
2024-07-19 23:58:07 +0900

    240719

---
+++ algorithm-java/src/linear/lt771.java
@@ -0,0 +1,22 @@
+package linear;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class lt771 {
+    public int numJewelsInStones(String jewels, String stones) {
+        Map<Character, Boolean> hashMap = new HashMap<>();
+        for (char jewel : jewels.toCharArray()) {
+            hashMap.put(jewel, true);
+        }
+
+        int count = 0;
+        for (char stone : stones.toCharArray()) {
+            if (hashMap.containsKey(stone)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
