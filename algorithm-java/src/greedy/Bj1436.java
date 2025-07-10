c5846085d98b026648a64ee9ce9ff5e37bba2333
ooni <gdbsrjsdn@gmail.com>
2025-07-10 22:53:53 +0900

    250708

---
+++ algorithm-java/src/greedy/Bj1436.java
@@ -0,0 +1,26 @@
+package greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Bj1436 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int count = 0;
+        int num = 666;
+
+        while(true) {
+            if (String.valueOf(num).contains("666")) {
+                count++;
+                if (count == N) {
+                    System.out.println(num);
+                    break;
+                }
+            }
+            num++;
+        }
+    }
+}
